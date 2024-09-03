6d222b95d766a81819bb6889a1c7d925b6a4afb3
gamassss <gamassakbar@gmail.com>
2024-09-04 00:35:37 +0700

    chore: update app config

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 import express, {Express, Request, Response} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config()
-console.log(process.env)
-const app: Express = express()
-const port = 5000
+// init dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(cors())
+dotenv.config({path: path.resolve(__dirname, '../.env.local')});
 
+const app: Express = express();
+const port: string = process.env.PORT || '5000';
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!")
+  res.send("hello world")
 })
 
 app.listen(port, () => {
-  console.log(`Server started on port 5000`)
+  console.log(`Server running on port ${port}`)
 })
